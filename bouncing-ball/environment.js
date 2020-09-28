6cf064ced063654b1c293a7214f490b026cd287b
Mia <maria.riezebos@gmail.com>
2020-09-28 17:06:25 +0200

    Create environment.js
    a simple file for environment variables

---
+++ bouncing-ball/environment.js
@@ -0,0 +1,18 @@
+// == OPTIONS ==
+
+// Framerate
+export let framerate = 60;
+
+// Canvas
+export let canvasWidth = 1280;
+export let canvasHeight = 720;
+// Gravitational acceleration (0 to disable completely)
+export let g = 0;
+
+// Bounciness (restitution coëfficient) (0-1)
+export let b = 1.2;
+
+// Velocity
+export let v = 5;
+
+// =============
