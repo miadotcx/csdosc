544a245f0769687a930dbd4bb66e42d2ef3ea809
Mia <maria.riezebos@gmail.com>
2020-10-01 15:50:20 +0200

    Update environment.js

--- bouncing-ball/environment.js
+++ bouncing-ball/environment.js
@@ -1,18 +1,18 @@
 // == OPTIONS ==
 
 // Framerate
-export let framerate = 60;
+export let framerate = 288;
 
 // Canvas
-export let canvasWidth = 1280;
-export let canvasHeight = 720;
+export let canvasWidth = window.innerWidth;
+export let canvasHeight = window.innerHeight;
 // Gravitational acceleration (0 to disable completely)
-export let g = 1;
+export let g = 1.5;
 
 // Bounciness (restitution coëfficient) (0-1)
-export let b = 0.75;
+export let b = 0.65;
 
 // Velocity
-export let v = 300;
+export let v = 200;
 
 // =============
